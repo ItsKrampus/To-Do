a3fc201d8a5e9e150afff6cefcb5672783bbbf99
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2024-02-03 15:44:13 +0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
 import './App.css'
-
+import TodoList from './TodoList'
+import { CssBaseline } from '@mui/material'
+import Navbar from './Navbar'
 function App() {
 
   return (
     <>
-   
+    <CssBaseline />
+    <Navbar />
+    <TodoList />
     </>
   )
 }
